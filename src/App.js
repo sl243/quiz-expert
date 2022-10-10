86539d3afc9aa972e0aaed53da6172dd91cf5b4b
sl243 <slmiucse@gmail.com>
2022-10-10 21:35:54 +0600

    create router and added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Nav></Nav>,
+      children: [
+        {
+          path: '/',
+          element: <Header></Header>
+        },
+        {
+          path: 'home',
+          element: <Home></Home>
+        }
+      ]
+    }
+  ])
+
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
