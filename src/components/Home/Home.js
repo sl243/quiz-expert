cca5ae238e3b0f9b69e549e6150dbf797e7b0b55
sl243 <slmiucse@gmail.com>
2022-10-12 15:51:50 +0600

    responsive home components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,43 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className='container'>
-            <h1>This is Home</h1>
-            <div className='header-image'>
+        <div className='container d-flex mt-5 header-section'>
+            <div className='header-image mt-4'>
                 <img src='https://i.ibb.co/G2Y2LPN/PYS-Hazelwood-Jaf-0013.jpg' alt=''></img>
             </div>
-        </div>
+            <div className='p-3 m-0'>
+                <h4>
+                    Quiz A contest in which participants test what they know by answering questions on one or more topics.
+                </h4>
+                <p className='fs-4 m-0'>
+                    1. Quizzes are fun
+                </p>
+                <p className='fs-4 m-0'>
+                    2. Quizzes raise confidence
+                </p>
+                <p className='fs-4 m-0'>
+                    3. Quizzes for children can aid planning and identify progress
+                </p>
+                <p className='fs-4 m-0'>
+                    4. Quizzes can support individualised learning
+                </p>
+                <p className='fs-4 m-0'>
+                    5. Quizzes are great for plenaries
+                </p>
+                <p className='fs-4 m-0'>
+                    6. Quizzes are great for end of topic assessments
+                </p>
+                <p className='fs-4 m-0'>
+                    7. Quizzes are good for revision
+                </p>
+                <p className='fs-4 m-0'>
+                    8. Quizzes can help tracking
+                </p>
+                <p className='fs-4 m-0'>
+                    9. Quizzes encourage pupils’ self awareness of progress and self assessment
+                </p>
+            </div>
+        </div >
     );
 };
 
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -9,7 +9,7 @@ const Topic = ({ topic }) => {
     const {id, name, logo, total } = topic;
     return (
         <div className='border rounded-3 shadow p-3 mb-5 bg-body'>
-            <div className='container topic-container bg-primary'>
+            <div className='container topic-container bg-primary border rounded'>
                 <img src={logo} alt=''></img>
             </div>
             <div className='d-flex gap-4 mt-5'>
